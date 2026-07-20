15227a003d81d635608c54b5d59b6209a3d5fa6f
fazy777 <hafizmfaizanali26688@gmail.com>
2026-07-20 18:36:25 +0500

    feat: add backend spring boot and frontend react vite project setup

---
+++ backend/main_incident_dashboard/src/main/java/com/incident_managment/main_incident_dashboard/MainIncidentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.incident_managment.main_incident_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainIncidentDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainIncidentDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/main_incident_dashboard/src/test/java/com/incident_managment/main_incident_dashboard/MainIncidentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incident_managment.main_incident_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainIncidentDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
